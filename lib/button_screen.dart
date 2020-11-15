8734838372219070cb0ce9f479b0cae4f844bf5e
KarthikD77 <raviramkarthik7@gmail.com>
2020-11-15 14:11:55 +0530

    First screen

---
+++ lib/button_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class ButtonScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF9ad3bc),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:emergency_text/button_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String phoneNo;
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ButtonScreen()),
+            );
+          },
+          backgroundColor: Colors.black,
+          child: Icon(Icons.arrow_forward),
+        ),
+        backgroundColor: Color(0xFF9ad3bc),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200.0,
+              ),
+              TextField(
+                cursorColor: Colors.black,
+                autofocus: true,
+                onChanged: (value) {
+                  phoneNo = value;
+                },
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  hintText: "Enter your emergency contact",
+                  hintStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
