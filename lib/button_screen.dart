3ef9129643c9014429abac42103c704cb1858491
KarthikD77 <raviramkarthik7@gmail.com>
2020-11-15 14:32:21 +0530

    final screen added

--- lib/button_screen.dart
+++ lib/button_screen.dart
@@ -4,6 +4,17 @@ class ButtonScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF9ad3bc),
+      body: Column(
+        children: [
+          FlatButton(
+            color: Colors.black,
+            child: Text('Help',style: TextStyle(color: Colors.white),),
+            onPressed: (){
+              
+            },
+          ),
+        ],
+      ),
     );
   }
 }
